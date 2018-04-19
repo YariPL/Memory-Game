6395ff58b4076c9d990c9fdc5890b0b4cc703bae
Yaroslav <yaroslav.khudchenko@gmail.com>
2018-04-19 20:36:18 +0200

    created an arrays for opened cards

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const structure = document.querySelector('#structure')
-console.log(structure);
-  structure.addEventListener('click', function (event) {
-  if(event.target.nodeName === 'LI') {
+
+const structure = document.querySelector('#structure');
+let arr = [];
     
-    event.target.setAttribute('style', 'width:500px');  
-  } else{
-    console.log('fuuccckkkkk');
-  }
-});
+    structure.addEventListener('click', function (event) {
+        if(event.target.nodeName === 'LI') {
+       event.target.classList.add('show', 'open');
+          //unshift the first element
+          //check if the first element is equal to the 
+          
+          if(arr[0]===event.target.innerHTML || arr.length <1){
+              arr.unshift(event.target.innerHTML);
+              } else {
+            event.target.classList.remove('show', 'open');
+          }
+        }
+    console.log(arr);
+    });
+
