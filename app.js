4f909bf496ad8119d67681f06133ce0c99833480
Yaroslav <yaroslav.khudchenko@gmail.com>
2018-04-19 11:41:23 +0200

    rewrite js file
    Added event delegation function on the #structure element

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-let card = document.querySelector('.structure');
-console.log(card);
-
+const structure = document.querySelector('#structure')
+console.log(structure);
+  structure.addEventListener('click', function (event) {
+  if(event.target.nodeName === 'LI') {
+    
+    event.target.setAttribute('style', 'width:500px');  
+  } else{
+    console.log('fuuccckkkkk');
+  }
+});
