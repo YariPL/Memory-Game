a394fd908c643984218ec95e4fc3e0cffd2e986d
Yaroslav <yaroslav.khudchenko@gmail.com>
2018-04-19 20:43:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ let arr = [];
     structure.addEventListener('click', function (event) {
         if(event.target.nodeName === 'LI') {
        event.target.classList.add('show', 'open');
-          //unshift the first element
-          //check if the first element is equal to the 
+         
           
           if(arr[0]===event.target.innerHTML || arr.length <1){
               arr.unshift(event.target.innerHTML);
