53bbb5e528040b9951b919b4ceea1686252c38da
Yaroslav <yaroslav.khudchenko@gmail.com>
2018-04-22 21:31:06 +0200

    added several functions
    added shuffle function - new grid 
    added new game function - new grid order on reload
    added reload button function - new grid order on reload-button click

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
+///shuffle function
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
-let arr = [];
-$( ".card" ).click(function() {
-  $( this ).addClass( "show open" );
-});
-  
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+///new board when page loads
+function newGame(){
+	cards = shuffle(cards)
+	for (let i = 0; i < cards.length; i++){
+		structure.innerHTML = '';
+		[].forEach.call(cards, function(item){
+			structure.appendChild(item);
+		});
+		cards[i].classList.remove("show", "open", "match", "disabled");
+	}
+}
+
+//array of cards
+let card = document.getElementsByClassName('card');
+let cards = [...card];
+
+///smth
+const structure = document.querySelector('#structure');
+console.log(structure);
+
+
+
+///reload board
+$('.restart').on('click', function reload (){
+  newGame();
+})
+
+///new game on page load
+document.body.onload = newGame();
