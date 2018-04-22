4af59c9a94c9a54ff4e54ff7d7404ae689443c5b
Yaroslav <yaroslav.khudchenko@gmail.com>
2018-04-22 14:11:18 +0200

    add jquery to html. change dom methods to jquery

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
 
-const structure = document.querySelector('#structure');
 let arr = [];
-    
-    structure.addEventListener('click', function (event) {
-        if(event.target.nodeName === 'LI') {
-       event.target.classList.add('show', 'open');
-         
-          
-          if(arr[0]===event.target.innerHTML || arr.length <1){
-              arr.unshift(event.target.innerHTML);
-              } else {
-            event.target.classList.remove('show', 'open');
-          }
-        }
-    console.log(arr);
-    });
-
+$( ".card" ).click(function() {
+  $( this ).addClass( "show open" );
+});
+  
